e6f87f1b0275ebfd631b716d59d66a6f31c505fe
Frederik Granna <webmaster@granna.de>
2013-06-06 21:58:29 +0200

    added config.h & switches for jeenode

---
+++ config.h
@@ -0,0 +1,25 @@
+#define DEMOTIME 30000  // 30 seconds max on each demo is enough.
+#define DISPDELAY 100    // Each "display" lasts this long
+#define LONGDELAY 1000  // This delay BETWEEN demos
+#define DEFAULTTEXTSCROLLTIME 50
+#define PONGSPEEDMS 20//35
+
+#define FADEDELAY 60                     // Time to fade display to black
+#define SHOWCLOCK 5000                   // How long to show clocktype for
+#define MAX_CLOCK_MODE 5                 // Number of clock modes
+
+#define BAT1_X 2                         // Pong left bat x pos (this is where the ball collision occurs, the bat is drawn 1 behind these coords)
+#define BAT2_X 45                        // Pong right bat x pos (this is where the ball collision occurs, the bat is drawn 1 behind these coords)
+
+
+#ifdef JEENODE
+  #define CLKPIN 14
+  #define DATAPIN 4
+  #define CS1PIN 3
+  #define CS2PIN 7
+#else
+  #define CS1PIN 10
+  #define CS2PIN 11
+  #define CLKPIN 12
+  #define DATAPIN 13
+#endif
--- pongclock.ino
+++ pongclock.ino
@@ -2,28 +2,22 @@
 /// Hooking up a DS1307 (5V) or DS1340Z (3V) real time clock via I2C.
 // 2009-09-17 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
 
-// the real-time clock is connected to port 1 in I2C mode (AIO = SCK, dIO = SDA)
 
+//#define JEENODE
+  
+
+
+// the real-time clock is connected to port 1 in I2C mode (AIO = SCK, dIO = SDA)
+#include "config.h"
 #include <JeeLib.h>
 #include "MatrixDisplay.h"
 #include "DisplayToolbox.h"
 #include "Clockfont.h"
 #include "fontLarge.h"
-#define DEMOTIME 30000  // 30 seconds max on each demo is enough.
-#define DISPDELAY 100    // Each "display" lasts this long
-#define LONGDELAY 1000  // This delay BETWEEN demos
-#define DEFAULTTEXTSCROLLTIME 50
-#define PONGSPEEDMS 20//35
+
 // Macro to make it the initDisplay function a little easier to understand
 #define setMaster(dispNum, CSPin) initDisplay(dispNum,CSPin,true)
 #define setSlave(dispNum, CSPin) initDisplay(dispNum,CSPin,false)
-
-#define FADEDELAY 60                     // Time to fade display to black
-#define SHOWCLOCK 5000                   // How long to show clocktype for
-#define MAX_CLOCK_MODE 5                 // Number of clock modes
-
-#define BAT1_X 2                         // Pong left bat x pos (this is where the ball collision occurs, the bat is drawn 1 behind these coords)
-#define BAT2_X 45                        // Pong right bat x pos (this is where the ball collision occurs, the bat is drawn 1 behind these coords)
 
 PortI2C myport (3 /*, PortI2C::KHZ400 */);
 DeviceI2C rtc (myport, 0x68);
@@ -32,7 +26,7 @@ BlinkPlug buttons (2);
 MilliTimer everySecond;
 
 // Init Matrix
-MatrixDisplay disp(2,14,4, true);
+MatrixDisplay disp(2, CLKPIN, DATAPIN, true);
 // Pass a copy of the display into the toolbox
 DisplayToolbox toolbox(&disp);
 
@@ -61,12 +55,11 @@ void setup() {
   X_MAX = disp.getDisplayCount() * (disp.getDisplayWidth());
   Y_MAX = disp.getDisplayHeight();
   // Prepare displays
-  disp.setMaster(0,3); // INT
-  disp.setSlave(1,7); // P4, DATA
+  disp.setMaster(0,CS1PIN); // INT
+  disp.setSlave(1,CS2PIN); // P4, DATA
   getDate(time);
   mode_time_up = time[3];                 // Set first time to change clock mode
   //setDate(13,05,25,7,22,52,0);
-  
   uartMsgTemp.reserve(200);
 }
 
